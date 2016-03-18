c2190920af3d2e4c2a35cdfe8914ca3ee5ef5cf0
liubingfeng93@gmail.com <liubingfeng93@gmail.com>
2016-03-18 23:54:06 +1100

    java server

---
+++ WaTwEat_Server/src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by liubingfeng on 18/03/2016.
+ */
+import static spark.Spark.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        get("/", (req, res) -> "Hello, World!");
+    }
+}
