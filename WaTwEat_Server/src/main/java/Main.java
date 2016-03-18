6289093367e903648c5d6867dac9c61b35ebe72a
liubingfeng93@gmail.com <liubingfeng93@gmail.com>
2016-03-19 09:23:09 +1100

    java server test push

--- WaTwEat_Server/src/main/java/Main.java
+++ WaTwEat_Server/src/main/java/Main.java
@@ -1,12 +1,45 @@
 /**
  * Created by liubingfeng on 18/03/2016.
  */
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.google.gson.Gson;
+
+
 import static spark.Spark.*;
 public class Main
 {
     public static void main(String[] args)
     {
-        get("/", (req, res) -> "Hello, World!");
+        Gson gson = new Gson();
+        Log.setLogLevel(Log.DEBUG);
+        Log.logInfo(Main.class,"Test Debugging Log");
+
+        post("/client_json", "application/json", (request, response) ->
+        {
+            System.out.println("in here => " + request.body());
+            return request.body();
+        });
+
+
+
+        get("/hello", (request, response) -> new TestInfor(),gson::toJson);
+
+
+
+
         //testing pushing
+        String url = "https://maps.googleapis.com/maps/api/geocode/json";
+        HttpResponse<JsonNode> jsonResponse;
+        try {
+            jsonResponse = Unirest.get(url)
+                    .header("accept", "application/json")
+                    .queryString("address", "1600")
+                    .asJson();
+            System.out.println(jsonResponse.getBody().getObject().getJSONArray("results"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
