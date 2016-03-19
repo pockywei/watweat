e71f710db0bf3f1daf8a1cc5526cc3dba501560c
reginazhang <405714219@qq.com>
2016-03-19 12:51:18 +1100

    new branch

---
+++ watweat/client.swift
@@ -0,0 +1,14 @@
+//
+//  client.swift
+//  watweat
+//
+//  Created by Regina Zhang on 3/19/16.
+//  Copyright © 2016 WEI. All rights reserved.
+//
+
+import Foundation
+
+
+class Client {
+    func 
+}
