c797b5c58a1e584f1a5ae0d6b1fd7a92e40fec5b
reginazhang <405714219@qq.com>
2016-03-19 17:50:41 +1100

    added Client

--- watweat/Client.swift
+++ watweat/Client.swift
@@ -7,8 +7,29 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
+import Alamofire_SwiftyJSON
 
 
 class Client {
-    func 
+    //let id:Int
+    let alias:String
+    
+    init() {
+        //id = 1;
+        alias = "Xiao Fang"
+    }
+    
+    func sendRequest(url:NSURL, params:[String:AnyObject], method:Alamofire.Method) {
+        Alamofire.request(method, url, parameters: params)
+            .responseSwiftyJSON({ (request, response, json, error) in
+                print(json)
+                print(error)
+                //let street:String = json["array"][0]["street"].string!
+                //print(street)
+            })
+    }
+    
+    func going()
 }
