afac707a9d750e724bb3334a9582fed6a7a605a0
lsyssso <stewart_liang@hotmail.com>
2016-03-19 21:08:15 +1100

    6

--- watweat/ResdetailViewController.swift
+++ watweat/ResdetailViewController.swift
@@ -11,8 +11,22 @@ import UIKit
 import MapKit
 
 class ResdetailViewController:UIViewController{
+    
+    var resta: Artwork?
 	
+    @IBOutlet var categoryDetail: UITextView!
+    @IBOutlet var addressDetail: UITextView!
+    @IBOutlet var phoneDetail: UITextView!
 	
 	@IBOutlet weak var resMap: MKMapView!
+    @IBAction func considerBtnClick() {
+    }
 	
+    @IBAction func goBtnClick() {
+    }
+    
+    override func viewDidLoad() {
+        self.addressDetail.text = resta?.locationName
+        self.phoneDetail.text = String(resta?.phoneNumber)
+    }
 }
