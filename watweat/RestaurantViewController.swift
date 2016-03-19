2b0f54a6d4b3ec482e7d2f486d89a1e119cf0a59
Your Name <WEI>
2016-03-19 13:53:29 +1100

    change the item bar bug

--- watweat/RestaurantViewController.swift
+++ watweat/RestaurantViewController.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 import MapKit
+import AddressBook
 
 class RestaurantViewController: UIViewController {
 	
@@ -17,7 +18,23 @@ class RestaurantViewController: UIViewController {
 	var annotation: MKPointAnnotation?
 	var myLocation = CLLocation()
 	
+	
 	@IBOutlet weak var resMap: MKMapView!
+	
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		let artwork = Artwork(title: "Your match location",locationName: ("Restaurant"),
+			discipline: "Sculpture",coordinate: CLLocationCoordinate2D(latitude: Double((-37.8132)), longitude: Double((144.963))))
+		
+		resMap.addAnnotation(artwork)
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 }
